fd65a4213e2b5c82833f7b2536f77a68edddf659
mehranGulzar <m.gulzar@web.de>
2022-07-27 15:46:10 +0200

    add EmployeeService Class

---
+++ backend/src/main/java/de/neuefische/cgnjava222/zoomanagement/zoo/service/EmployeeService.java
@@ -0,0 +1,28 @@
+package de.neuefische.cgnjava222.zoomanagement.zoo.service;
+
+import de.neuefische.cgnjava222.zoomanagement.zoo.employee.model.Employee;
+import de.neuefische.cgnjava222.zoomanagement.zoo.employee.repository.EmployeeRepo;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+
+        EmployeeRepo employeeRepo;
+
+        public EmployeeService(EmployeeRepo employeeRepo) {
+                this.employeeRepo = employeeRepo;
+        }
+
+        public Employee getEmployee(String id) {
+                return employeeRepo.getEmployee(id);
+        }
+
+        public List<Employee> getListEmployees() {
+                return employeeRepo.listEmployees();
+        }
+
+
+}
