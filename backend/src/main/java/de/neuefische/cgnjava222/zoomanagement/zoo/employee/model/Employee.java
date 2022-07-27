8f15e6822b3357de89e5df4387b4cf28920d0a33
mehranGulzar <m.gulzar@web.de>
2022-07-27 15:38:54 +0200

    add EmployeeRepo Class

--- backend/src/main/java/de/neuefische/cgnjava222/zoomanagement/zoo/employee/model/Employee.java
+++ backend/src/main/java/de/neuefische/cgnjava222/zoomanagement/zoo/employee/model/Employee.java
@@ -9,11 +9,6 @@ public record Employee(
 
 ) {
 
-    public Employee(String name, String id) {
-        this.name = name;
-        this.id = id;
-    }
-
 
     public Employee(String name) {
         this(name, UUID.randomUUID().toString());
