7bc40b1e28a83af5f92b0026d0c3e16e9f1618a6
Daniel Schwarz <daniel.schwarz@neuefische.de>
2022-07-29 10:30:08 +0200

    Cleanup code

--- backend/src/main/java/de/neuefische/cgnjava222/zoomanagement/zoo/trucks/TruckRepo.java
+++ backend/src/main/java/de/neuefische/cgnjava222/zoomanagement/zoo/trucks/TruckRepo.java
@@ -1,9 +1,7 @@
 package de.neuefische.cgnjava222.zoomanagement.zoo.trucks;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface TruckRepo extends MongoRepository<Truck, String> {
 
 }
