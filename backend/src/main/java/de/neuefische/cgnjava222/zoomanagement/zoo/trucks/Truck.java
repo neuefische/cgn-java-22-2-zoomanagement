e91937c4e18e2f8206f6df45279ad21381574f73
merleandrade <andrade.merle@gmail.com>
2022-07-29 10:23:01 +0200

    Delete unused methode in truck

--- backend/src/main/java/de/neuefische/cgnjava222/zoomanagement/zoo/trucks/Truck.java
+++ backend/src/main/java/de/neuefische/cgnjava222/zoomanagement/zoo/trucks/Truck.java
@@ -2,11 +2,6 @@ package de.neuefische.cgnjava222.zoomanagement.zoo.trucks;
 
 import org.springframework.data.annotation.Id;
 
-import java.util.UUID;
-
 public record Truck(String name, @Id String id) {
 
-    public static Truck of(String name) {
-        return new Truck(name, String.valueOf(UUID.randomUUID()));
-    }
 }
