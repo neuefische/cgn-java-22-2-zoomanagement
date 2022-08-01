a5058cb6199d558ceb53fdfb5418eda5f9de0b2c
KSVoss <Kai.S.Voss@t-online.de>
2022-08-01 17:02:55 +0200

    add deleteTruck

--- backend/src/main/java/de/neuefische/cgnjava222/zoomanagement/zoo/truck/TruckController.java
+++ backend/src/main/java/de/neuefische/cgnjava222/zoomanagement/zoo/truck/TruckController.java
@@ -1,6 +1,7 @@
 package de.neuefische.cgnjava222.zoomanagement.zoo.truck;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,5 +26,18 @@ public class TruckController {
     @ResponseStatus(code = HttpStatus.CREATED)
     public Truck addTruck(@RequestBody NewTruck truck) {
         return truckService.addTruck(truck);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteTruck(@RequestBody String truckToDelete) {
+
+
+        if (truckService.deleteTruck(truckToDelete)) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+        }
+        return
+            ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+
+
     }
 }
