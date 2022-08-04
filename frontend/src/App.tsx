3f0db4c3f091883fb55d8888d6624af7e7534374
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2022-08-04 17:34:09 +0200

    Update positioning employees with shared Home and Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import './App.css';
-import usePlants from "./plant/usePlants";
-import PlantList from "./plant/PlantList";
-import TruckGallery from "./truck/TruckGallery";
-import useTrucks from "./truck/useTrucks";
-import Employees from "./employees/Employees";
-import useEmployee from "./employees/useEmployee";
-import useAnimals from "./animal/useAnimals";
-import AnimalList from "./animal/AnimalList";
-import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import {HashRouter} from "react-router-dom";
 import AllRoutes from "./shared/AllRoutes";
+import Header from "./shared/Header";
+import useAnimals from "./animal/useAnimals";
+import usePlants from "./plant/usePlants";
+import useTrucks from "./truck/useTrucks";
+import useEmployee from "./employees/useEmployee";
 
 
 export default function App() {
@@ -22,14 +18,12 @@ export default function App() {
     const {employees, addEmployee, deleteEmployee} = useEmployee();
 
     return <>
-        <h1>Zoo-Management</h1>
+        <Header/>
         <HashRouter>
-            <AllRoutes/>
-            <PlantList plants={plants} addPlant={addPlant}/>
-            <TruckGallery trucks={trucks} addTruck={addTruck}/>
-            <AnimalList animals={animals} addAnimal={addAnimal} onDeleteAnimal={onDeleteAnimal}/>
-            <Employees employees={employees} addEmployee={addEmployee} onDeleteEmployee={deleteEmployee}/>
-            <ToastContainer/>
+            <AllRoutes animals={animals} addAnimal={addAnimal} onDeleteAnimal={onDeleteAnimal}
+                       plants={plants} addPlant={addPlant}
+                       trucks={trucks} addTruck={addTruck}
+                       employees={employees} addEmployee={addEmployee} deleteEmployee={deleteEmployee}/>
         </HashRouter>
     </>;
 
--- frontend/src/shared/AllRoutes.tsx
+++ frontend/src/shared/AllRoutes.tsx
@@ -1,13 +1,20 @@
 import DetailEmployee from "../employees/DetailEmployee";
 import React from "react";
 import {Route, Routes} from "react-router-dom";
+import Home from "./Home";
 
-export default function AllRoutes() {
+export default function AllRoutes({...props}) {
 
     return (
 
         <Routes>
-            <Route path={"employees/:id"} element={<DetailEmployee/>}/>
+            <Route path={"/"} element={<Home animals={props.animals} addAnimal={props.addAnimal}
+                                             onDeleteAnimal={props.onDeleteAnimal}
+                                             plants={props.plants} addPlant={props.addPlant}
+                                             trucks={props.trucks} addTruck={props.addTruck}
+                                             employees={props.employees} addEmployee={props.addEmployee}
+                                             deleteEmployee={props.deleteEmployee}/>}/>
+            <Route path={"employees/:id"} element={<DetailEmployee employees={props.employees}/>}/>
         </Routes>
 
     )
