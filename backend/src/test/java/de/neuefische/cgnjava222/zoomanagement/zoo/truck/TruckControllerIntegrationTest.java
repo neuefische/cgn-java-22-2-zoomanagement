a9defbefa93ecb8929b57bf6cd5af11b3f092f60
sarakrieger <sara.krieger2022@gmail.com>
2022-08-04 16:23:36 +0200

    Add deleteTruck tests

--- backend/src/test/java/de/neuefische/cgnjava222/zoomanagement/zoo/truck/TruckControllerIntegrationTest.java
+++ backend/src/test/java/de/neuefische/cgnjava222/zoomanagement/zoo/truck/TruckControllerIntegrationTest.java
@@ -1,19 +1,20 @@
 package de.neuefische.cgnjava222.zoomanagement.zoo.truck;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -21,6 +22,9 @@ class TruckControllerIntegrationTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
 
 
     @Test
@@ -56,4 +60,31 @@ class TruckControllerIntegrationTest {
         String content = result.getResponse().getContentAsString();
         Assertions.assertTrue(content.contains("Dner1"));
     }
+
+
+    @DirtiesContext
+    @Test
+    void deleteTrucks() throws Exception {
+
+        String saveResult = mockMvc.perform(post(
+                "/api/trucks")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {"name": "Foodtruck"}
+                        """)
+        ).andReturn().getResponse().getContentAsString();
+
+        Truck saveResultTruck = objectMapper.readValue(saveResult, Truck.class);
+        String id = saveResultTruck.id();
+
+        mockMvc.perform(delete("http://localhost:8080/api/trucks/" + id))
+                .andExpect(status().is(204));
+
+        mockMvc.perform(get("http://localhost:8080/api/trucks"))
+                .andExpect(status().is(200))
+                .andExpect(content().json("""
+                        []
+                        """));
+    }
+
 }
