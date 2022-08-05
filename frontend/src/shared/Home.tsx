32187cf7a3d2edba0cecc889f47f45ee693f9a96
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2022-08-05 12:02:37 +0200

    Upgrade App.tsx, Home.tsx, AllRoutes.tsx

--- frontend/src/shared/Home.tsx
+++ frontend/src/shared/Home.tsx
@@ -4,14 +4,36 @@ import AnimalList from "../animal/AnimalList";
 import Employees from "../employees/Employees";
 import {ToastContainer} from "react-toastify";
 import React from "react";
+import {Animal} from "../animal/Animal";
+import {Plant} from "../plant/Plant";
+import Truck from "../truck/Truck";
+import {Employee} from "../employees/Employee";
 
-export default function Home({...props}) {
+type HomeProps = {
+    animalHook: {
+        animals: Animal[],
+        addAnimal: (animalName: string) => Promise<void>,
+        onDeleteAnimal: (id: string) => Promise<void>,
+    },
+    plantHook: {
+        plants: Plant[], addPlant: (name: string) => Promise<void>
+    },
+    truckHook: {
+        trucks: Truck[], addTruck: (name: string) => Promise<void>
+    },
+    employeeHook: {
+        deleteEmployee: (id: string) => Promise<void>, addEmployee: (newName: string) => Promise<any>, employees: Employee[]
+    },
+}
+export default function Home(props: HomeProps) {
 
     return (<>
-        <PlantList plants={props.plants} addPlant={props.addPlant}/>
-        <TruckGallery trucks={props.trucks} addTruck={props.addTruck}/>
-        <AnimalList animals={props.animals} addAnimal={props.addAnimal} onDeleteAnimal={props.onDeleteAnimal}/>
-        <Employees employees={props.employees} addEmployee={props.addEmployee} onDeleteEmployee={props.deleteEmployee}/>
+        <PlantList plants={props.plantHook.plants} addPlant={props.plantHook.addPlant}/>
+        <TruckGallery trucks={props.truckHook.trucks} addTruck={props.truckHook.addTruck}/>
+        <AnimalList animals={props.animalHook.animals} addAnimal={props.animalHook.addAnimal}
+                    onDeleteAnimal={props.animalHook.onDeleteAnimal}/>
+        <Employees employees={props.employeeHook.employees} addEmployee={props.employeeHook.addEmployee}
+                   onDeleteEmployee={props.employeeHook.deleteEmployee}/>
         <ToastContainer/>
     </>);
 }
