3f61af6f560fb478f93273a4e57709a92ce09c25
sarakrieger <sara.krieger2022@gmail.com>
2022-08-07 14:06:58 +0200

    Added Koordinate labels and speichern button

--- frontend/src/truck/FoodTruckDetail.tsx
+++ frontend/src/truck/FoodTruckDetail.tsx
@@ -8,12 +8,19 @@ export default function FoodTruckDetail(props: FoodTruckDetailsProps) {
 
     const {id} = useParams();
     const truck: Truck | undefined = props.trucks.find((e: Truck) => e.id === id);
-    console.log(id, truck, props.trucks)
+
 
     return (
         <>
             <h2>{truck?.name}</h2>
+            <form>
+                <label> X - Koordinate : </label> <br/>
+                <br/>
+                <label>Y - Koordinate : </label> <br/>
+                <br/>
+                <button>speichern</button>
 
+            </form>
 
         </>
     )
