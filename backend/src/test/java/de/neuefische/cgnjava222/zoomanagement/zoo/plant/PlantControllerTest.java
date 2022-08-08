6d8f034cc84b0581a079b9ba72756f8a4c3a1fa0
asrarbutt <asrarbutt1986@gmail.com>
2022-08-08 09:52:54 +0200

    changes after review

--- backend/src/test/java/de/neuefische/cgnjava222/zoomanagement/zoo/plant/PlantControllerTest.java
+++ backend/src/test/java/de/neuefische/cgnjava222/zoomanagement/zoo/plant/PlantControllerTest.java
@@ -2,8 +2,6 @@ package de.neuefische.cgnjava222.zoomanagement.zoo.plant;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.test.annotation.DirtiesContext;
-
 import java.util.List;
 import java.util.UUID;
 
