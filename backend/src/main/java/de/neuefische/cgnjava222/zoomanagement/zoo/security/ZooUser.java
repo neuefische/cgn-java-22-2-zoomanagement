5ae752ace9f23d4093b3dae7cde120e7a94521cc
asrarbutt <asrarbutt1986@gmail.com>
2022-08-10 15:36:54 +0200

    add spring security config

---
+++ backend/src/main/java/de/neuefische/cgnjava222/zoomanagement/zoo/security/ZooUser.java
@@ -0,0 +1,13 @@
+package de.neuefische.cgnjava222.zoomanagement.zoo.security;
+
+import org.springframework.data.annotation.Id;
+
+public record ZooUser(
+
+        @Id
+        String username,
+        String password
+
+) {
+
+}
