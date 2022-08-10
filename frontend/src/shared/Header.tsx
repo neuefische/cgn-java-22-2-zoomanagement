8c525e722d54d30efbd2f2597d020badc4bcaf3a
davidebschke <Davidebschke8@gmail.com>
2022-08-10 16:04:30 +0200

    Navlink onclick tabName Change, Tabpicture

--- frontend/src/shared/Header.tsx
+++ frontend/src/shared/Header.tsx
@@ -1,17 +1,22 @@
 import {NavLink} from "react-router-dom";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./header.css";
 
 export default function Header() {
+    const [tab, setTab] = useState("Home");
+
+    useEffect(() => {
+        document.title = tab;
+    });
     return (
         <>
             <h1> Zoo-Management</h1>
             <div className="menu">
-            <NavLink className={"nav"} to={'/'}><img src={"../pictures/home.png"} alt={"Home"}/></NavLink>
-            <NavLink className={"nav"} to={'/Animals'}><img src={"../pictures/lion.png"} alt ={"Animals"}/></NavLink>
-            <NavLink className={"nav"} to={'/Trucks'}><img src={"../pictures/truck.png"} alt={"Trucks"}/></NavLink>
-            <NavLink className={"nav"} to={'/Employees'}><img src={"../pictures/employees.png"} alt={"Employees"}/></NavLink>
-            <NavLink className={"nav"} to={'/Plants'}><img src={"../pictures/plant.png"} alt={"Plants"}/></NavLink>
+            <NavLink className={"nav"} onClick={()=>setTab("Home")} to={'/'}><img src={"../pictures/home.png"} alt={"Home"}/></NavLink>
+            <NavLink className={"nav"} onClick={()=>setTab("Animals")} to={'/Animals'}><img src={"../pictures/lion.png"} alt ={"Animals"}/></NavLink>
+            <NavLink className={"nav"} onClick={()=>setTab("Trucks")} to={'/Trucks'}><img src={"../pictures/truck.png"} alt={"Trucks"}/></NavLink>
+            <NavLink className={"nav"} onClick={()=>setTab("Employees")} to={'/Employees'}><img src={"../pictures/employees.png"} alt={"Employees"}/></NavLink>
+            <NavLink className={"nav"} onClick={()=>setTab("Plants")} to={'/Plants'}><img src={"../pictures/plant.png"} alt={"Plants"}/></NavLink>
             </div>
         </>
     );
