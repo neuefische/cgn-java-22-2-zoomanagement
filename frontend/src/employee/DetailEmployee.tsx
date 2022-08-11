19e2088459be0c109726c4e71824c4b7f74e4142
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2022-08-11 23:37:15 +0200

    Corrections following Nicole's suggestions

--- frontend/src/employee/DetailEmployee.tsx
+++ frontend/src/employee/DetailEmployee.tsx
@@ -1,6 +1,7 @@
 import Employee from "./Employee";
 import {useParams} from "react-router-dom";
-import {useState} from "react";
+import {FormEvent, useEffect, useState} from "react";
+import {showError} from "./ErrorEmployee";
 
 type DetailEmployeesProps = {
     employees: Employee[],
@@ -13,28 +14,43 @@ export default function DetailEmployee(props: DetailEmployeesProps) {
     const [valueY, setValueY] = useState<string>("")
     const employee: Employee | undefined = props.employees.find((e: Employee) => e.id === id);
 
-    const handleUpdate = () => {
+    useEffect(() => {
+        setValueX(employee?.position?.x || "");
+        setValueY(employee?.position?.y || "")
+    }, [])
 
+
+    const handleUpdate = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
         if (employee) {
             const updatedEmployee: Employee = {name: employee.name, id: employee.id, position: {x: valueX, y: valueY}};
             props.employeeUpdate(updatedEmployee);
+        } else {
+            showError("Not found");
         }
+    }
 
+    if (!employee) {
+        return <>Mitarbeiter mit ID "{id}" leider nicht gefunden.</>;
     }
 
     return (<>
+
             <h2>{employee?.name}</h2>
-            <form>
-                <label htmlFor="xInput">X - Koordinate : <input id="xInput" type="text" value={valueX}
+            <form onSubmit={handleUpdate}>
+                <label htmlFor="xInput">X - Koordinate : <input id="xInput" type="text"
+                                                                defaultValue={valueX}
                                                                 onChange={(event) => {
                                                                     setValueX(event.target.value);
                                                                 }}/></label>
-                <label htmlFor="yInput">Y - Koordinate : <input id="yInput" type="text" value={valueY}
+                <label htmlFor="yInput">Y - Koordinate : <input id="yInput" type="text"
+                                                                defaultValue={valueY}
                                                                 onChange={(event) => {
                                                                     setValueY(event.target.value);
                                                                 }}/></label>
-                <button type={"submit"} onClick={handleUpdate}>speichern</button>
+                <button type={"submit"}>speichern</button>
             </form>
+
         </>
     )
 }
