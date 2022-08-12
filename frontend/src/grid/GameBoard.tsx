7615c6c13dda6d3052784d511f5d58191ca99b9e
claudia <claudia.dreifke@gmail.com>
2022-08-12 13:03:01 +0200

    add grid (now functional)

--- frontend/src/grid/GameBoard.tsx
+++ frontend/src/grid/GameBoard.tsx
@@ -1,20 +1,29 @@
-import BoardModel from "./BoardModel";
 import {Cell} from "./Cell";
+import "./BoardModel.css";
 
-type GameBoardProps = {
-    cell: Cell,
-    gameCells: Cell[],
-}
+export default function GameBoard() {
 
-export default function GameBoard(props: GameBoardProps) {
+    const maxNumOfRows = 10;
+    const maxNumOfColumns = 10;
+    let gameCells: Cell[] = [];
+    let newCell: Cell;
+
+    for (let yCounter = 0; yCounter < maxNumOfRows; yCounter++) {
+        for (let xCounter = 0; xCounter < maxNumOfColumns; xCounter++) {
+            newCell = {
+                index: "" + yCounter + xCounter, position: {x: xCounter.toString(), y: yCounter.toString()},
+            }
+            gameCells.push(newCell);
+        }
+    }
 
     return (
         <>
             <h2>Game-Board</h2>
-            <ol>
-                {props.gameCells.map(cell => <li key={cell.index}><BoardModel/>
-                </li>)},
-            </ol>
+            <div className={"board"}>
+                {gameCells.map(cell => <div className={"cell"} key={cell.index}>
+                </div>)}
+            </div>
         </>
     )
 }
