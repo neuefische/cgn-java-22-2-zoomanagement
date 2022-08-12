e59b94deabf7cfd517c8be9fa5ebc75be70e0066
irenepapaspyratos <101578008+irenepapaspyratos@users.noreply.github.com>
2022-08-12 02:18:19 +0200

    Update Truck

--- frontend/src/truck/Truck.ts
+++ frontend/src/truck/Truck.ts
@@ -2,10 +2,8 @@ export type Truck = {
     name: string,
     id: string,
     position: { x: string, y: string } | undefined,
-
 }
 
-export type NewTruck = Omit<Truck, "id" | "position">
+export type NewTruck = Omit<Truck, "id" | "position">;
 
-
-export default Truck
+export default Truck;
