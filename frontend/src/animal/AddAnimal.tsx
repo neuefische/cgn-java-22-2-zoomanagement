fa5f7c103dcd13fa2352336f5017e72af6ffe3d6
davidebschke <Davidebschke8@gmail.com>
2022-08-12 16:37:56 +0200

    fix problems

--- frontend/src/animal/AddAnimal.tsx
+++ frontend/src/animal/AddAnimal.tsx
@@ -31,6 +31,7 @@ export default function AddAnimal(props: AddAnimalProps) {
     const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
         setAnimalName(event.target.value);
     }
+
     useEffect(() => {
         if (props.apiAnimals && props.apiAnimals.length > 0) {
             setAnimalName(props.apiAnimals[0]);
@@ -40,7 +41,7 @@ export default function AddAnimal(props: AddAnimalProps) {
     return (
         <form onSubmit={handleSubmit}>
 
-            <select value={animalName} onChange={handleChange}>
+            <select value={animalName} onChange={()=>handleChange}>
                 {props.apiAnimals.map(apiAnimal => (<option className="option" value={apiAnimal}>{apiAnimal}</option>))}
             </select>
 
