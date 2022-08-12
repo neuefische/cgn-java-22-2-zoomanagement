c3f0866e3bc83f5a7b2a6d811826ce6081b4031a
nicolekarakin <nicole.karakin@gmail.com>
2022-08-13 00:11:31 +0200

    adding mockUser to integration tests

--- backend/src/test/java/de/neuefische/cgnjava222/zoomanagement/zoo/employee/EmployeeControllerIntegrationTest.java
+++ backend/src/test/java/de/neuefische/cgnjava222/zoomanagement/zoo/employee/EmployeeControllerIntegrationTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -14,6 +15,7 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@WithMockUser(username = "wer")
 @SpringBootTest
 @AutoConfigureMockMvc
 class EmployeeControllerIntegrationTest {
--- backend/src/test/java/de/neuefische/cgnjava222/zoomanagement/zoo/truck/TruckControllerIntegrationTest.java
+++ backend/src/test/java/de/neuefische/cgnjava222/zoomanagement/zoo/truck/TruckControllerIntegrationTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -16,6 +17,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@WithMockUser(username = "wer")
 @SpringBootTest
 @AutoConfigureMockMvc
 class TruckControllerIntegrationTest {
