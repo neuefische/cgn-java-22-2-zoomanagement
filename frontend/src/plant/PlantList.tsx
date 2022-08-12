66ba6e204e8774cc5af5cf6aeafc3cdb9641df2e
sarakrieger <sara.krieger2022@gmail.com>
2022-08-12 17:09:18 +0200

    Add search forAll

--- frontend/src/plant/PlantList.tsx
+++ frontend/src/plant/PlantList.tsx
@@ -2,6 +2,9 @@ import AddPlant from "./AddPlant";
 import {PlantType} from "./PlantType";
 import Plant from "./Plant";
 import "../App.css"
+import {useState} from "react";
+import GeneralSearch from "../shared/GeneralSearch";
+import {filterGeneral} from "../shared/helpers";
 
 type PlantListProps = {
     plants: PlantType[],
@@ -11,13 +14,19 @@ type PlantListProps = {
 }
 export default function PlantList(props: PlantListProps) {
 
+    const [filterValue, setFilterValue] = useState<string>("");
+    const [radioValue, setRadioValue] = useState<string>("all");
+    const objectList = props.plants;
+
     return (
         <>
             <h2>Pflanzen</h2>
+            <GeneralSearch setFilterValue={setFilterValue} setRadioValue={setRadioValue}/>
             <ul>
-                {props.plants.map(plant => <li key={plant.id}>
-                    <Plant plant={plant} deletePlant={props.deletePlant}/>
-                </li>)}
+                {filterGeneral(filterValue, radioValue, objectList)
+                    .map(plant => <li key={plant.id}>
+                        <Plant plant={plant} deletePlant={props.deletePlant}/>
+                    </li>)}
             </ul>
             <AddPlant addPlant={props.addPlant} apiPlants={props.apiPlants}/>
         </>
--- frontend/src/truck/TruckGallery.tsx
+++ frontend/src/truck/TruckGallery.tsx
@@ -1,5 +1,8 @@
 import Truck from "./Truck";
 import AddTruck from "./AddTruck";
+import {useState} from "react";
+import GeneralSearch from "../shared/GeneralSearch";
+import {filterGeneral} from "../shared/helpers";
 
 
 type TruckGalleryProps = {
@@ -10,16 +13,23 @@ type TruckGalleryProps = {
 
 export default function TruckGallery(props: TruckGalleryProps) {
 
+    const [filterValue, setFilterValue] = useState<string>("");
+    const [radioValue, setRadioValue] = useState<string>("all");
+    const objectList = props.trucks;
+
+
     return (
         <>
             <h2>Food-Trucks</h2>
+            <GeneralSearch setFilterValue={setFilterValue} setRadioValue={setRadioValue}/>
 
             <ul>
-                {props.trucks
+                {filterGeneral(filterValue, radioValue, objectList)
                     .map(truck => <li key={truck.id}>
-                        <div className={"nameStyle"}>{truck.name}
-                            <button onClick={() => props.deleteTruck(truck.id)}>Löschen</button>
-                        </div></li>
+                            <div className={"nameStyle"}>{truck.name}
+                                <button onClick={() => props.deleteTruck(truck.id)}>Löschen</button>
+                            </div>
+                        </li>
                     )}
             </ul>
             <AddTruck addTruck={props.addTruck}/>
