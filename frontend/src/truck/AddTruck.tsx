bf93a5b27268090e37a4bf4c69ea85d375c4872b
irenepapaspyratos <101578008+irenepapaspyratos@users.noreply.github.com>
2022-08-12 02:08:27 +0200

    Update AddTruck

--- frontend/src/truck/AddTruck.tsx
+++ frontend/src/truck/AddTruck.tsx
@@ -2,33 +2,34 @@ import {ChangeEvent, FormEvent, useState} from "react";
 import {toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-
 type AddTruckProps = {
     addTruck: (name: string) => Promise<void>
 }
 export default function AddTruck(props: AddTruckProps) {
     const [truckName, setTruckName] = useState<string>("")
 
-
     const onNameChange = (event: ChangeEvent<HTMLInputElement>) => {
         setTruckName(event.target.value)
     }
+    
     const onTruckSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         if (truckName === "") return;
 
         props.addTruck(truckName)
             .catch((error) => {
-                notify("Truck kann nicht hinzugefügt werden! Datenbankfehler " + error.message)
+                notify("Truck kann nicht hinzugefügt werden! Datenbankfehler " + error.message);
             })
 
-        setTruckName("")
+        setTruckName("");
     }
+    
     const notify = (message: string) => {
         toast.error(message, {
             position: toast.POSITION.TOP_LEFT
         });
     };
+    
     return (
         <form onSubmit={onTruckSubmit}>
             <label>Neuen Truck hinzufügen
