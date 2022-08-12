d2657ba89afdbf090f470b7cadcef5335aa35f5c
matthiasboosch <matthias_boosch@yahoo.de>
2022-08-12 11:35:07 +0200

    Set back to working version.

--- frontend/src/shared/CheckPositionRules.tsx
+++ frontend/src/shared/CheckPositionRules.tsx
@@ -2,33 +2,43 @@ import Truck from "../truck/Truck";
 import {PlantType} from "../plant/PlantType";
 import Employee from "../employee/Employee";
 import {Animal} from "../animal/Animal";
-import useRules from "../rules/useRules";
+import {useEffect, useState} from "react";
 
-type checkPositionRulesProps = {
-    employees: Employee[],
-    animals: Animal[],
-    trucks: Truck[],
-    plants: PlantType[],
-    missingPositionPlant: PlantType[],
+type checkPositionRulesProps ={
+    trucks:Truck[],
+    plants:PlantType[],
+    employees:Employee[],
+    animals:Animal[],
 }
 export default function CheckPositionRules(props:checkPositionRulesProps){
 
-    useRules(props.employees, props.animals, props.trucks, props.plants)
+const [missingPositionPlant,setMissingPositionPlant]=useState<PlantType[]>([]);
 
-
-
-     return (
-
-         <>
-             <p>folgende Elemente haben keine Position:</p>
-             <ul>  {props.missingPositionPlant.map((element) => <li>{element.name}</li>)
-             }
-             </ul>
-         </>
-     )
+const addMissingPlant=(newPlant:PlantType)=>{
+    setMissingPositionPlant(state=>[...state,newPlant])
+}
+const checkPositionPlant=(element :PlantType)=> {
+    if (!element.position) {
+        addMissingPlant(element)
+    } else {
+        if (element.position.x === null || element.position.y === null) {
+            addMissingPlant(element)
+        }
+    }
 }
 
-//<useRules employees={props.employees}
-//             animals={props.animals}
-//             trucks={props.trucks}
-//             plants={props.plants}/>
+useEffect(()=>{
+    setMissingPositionPlant([]);
+    props.plants.forEach(checkPositionPlant);
+     },[props.plants]);
+
+     return(
+         <>
+             <p>folgende Elemente haben keine Position:</p>
+             <ul>
+                 {missingPositionPlant.map((element) => <li>{element.name}</li>)
+                 }
+             </ul>
+         </>
+    )
+}
